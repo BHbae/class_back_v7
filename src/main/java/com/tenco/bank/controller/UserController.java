eb3641cd6c4a4038945bf7e221a89ea6f9a73181
BHbae <majinga15@naver.com>
2024-08-07 11:18:23 +0900

    로그아웃 기능추가

--- src/main/java/com/tenco/bank/controller/UserController.java
+++ src/main/java/com/tenco/bank/controller/UserController.java
@@ -7,17 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tenco.bank.dto.SignInDTO;
 import com.tenco.bank.dto.SignUpDTO;
 import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.repository.model.User;
 import com.tenco.bank.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller // IoC에 대상(싱글톤 패턴으로 관리됨)
 @RequestMapping("/user") // 대문 처리
 public class UserController {
 	
-	@Autowired // DI 처리
 	private UserService userService;
+	private final HttpSession session;
 	
+	// DI 처리
+	@Autowired 
+	public UserController(UserService service, HttpSession session) {
+		this.userService = service;
+		this.session = session;
+	}
 	
 	
 	/**
@@ -58,8 +68,53 @@ public class UserController {
 		userService.createUser(dto);
 		
 		// TODO - 추후 수정
-		return "redirect:/index";
+		return "redirect:/user/sign-in";
+	}
+	/**
+	 * 로그인 화면 요청
+	 * 주소설계 : http://localhost:8080/user/sign-in
+	 * @return
+	 */
+	@GetMapping("/sign-in")
+	public String signIn() {
+		// 인증검사 x
+		// 유효성 검사 x
+		return "user/signIn";
 	}
 	
+	/**
+	 * 회원 가입 요청 처리
+	 * 주소 설계 : http://localhost:8080/user/sign-in
+	 * @return
+	 */
+	@PostMapping("/sign-in")
+	public String signProc(SignInDTO dto) {
+		// 1.인증 검사 x
+		// 2.유효성 검사 o
+		if(dto.getUsername() == null || dto.getUsername().isEmpty()) {
+			throw new DataDeliveryException("username 을 입력하세요", HttpStatus.BAD_REQUEST);
+		}
+		if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new DataDeliveryException("password 을 입력하세요", HttpStatus.BAD_REQUEST);
+		}
+		
+		// 서비스 호출
+		User principal = userService.readUser(dto);
+		
+		// 세션 메모리에 등록 처리
+		session.setAttribute("principal", principal);
+		
+		// 새로운 페이지로 이동 처리
+		// TODO - 계좌 목록 페이지 이동처리
+		return "redirect:/index";
+	} // end of signProc
+	
+	// 코드 추가
+	@GetMapping("/logout")
+	public String logout() {
+		session.invalidate(); // 로그아웃됨
+		
+		return "redirect:/user/sign-in";
+	}
 	
 }
